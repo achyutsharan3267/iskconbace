eabe90b7ff3ccc067e46b5f21496252e23f7240a
achyutsharan3267 <achyutsharan95@gmail.com>
2023-03-16 01:33:01 +0530

    hari bol

--- src/app/information/guru-parampara/guru-parampara.component.ts
+++ src/app/information/guru-parampara/guru-parampara.component.ts
@@ -9,4 +9,37 @@ export class GuruParamparaComponent {
   ngOnInit(): void {
     animationparampara();
   }
+  brahmasampradaya = [
+    {
+      GuruName: 'Brahma',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+    {
+      GuruName: 'Shiva',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+    {
+      GuruName: 'Narayana',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+    {
+      GuruName: 'Vsihnu',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+  ];
+
+  brahma = [
+    {
+      GuruName: 'Brahma',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+    {
+      GuruName: 'Shiva',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+    {
+      GuruName: 'Narayana',
+      GuruImage: 'https://wallpapers.com/images/featured/opvqvid74jpkwi9n.jpg',
+    },
+  ];
 }
