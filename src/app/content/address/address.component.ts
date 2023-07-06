53c6be50ba3987c90033d9a3c6e3269f32e859ff
achyutsharan3267 <achyut.sharan@impressico.com>
2023-07-06 18:45:34 +0530

    new header

--- src/app/content/address/address.component.ts
+++ src/app/content/address/address.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./address.component.scss']
 })
 export class AddressComponent {
-
+  memorypotrait = [
+    'https://i.postimg.cc/NFX7297m/IMG20230520191511.jpg',
+'https://i.postimg.cc/Jnv5hcJR/IMG20230520191518.jpg',
+'https://i.postimg.cc/0QbCxHzt/IMG20230520191528.jpg',
+'https://i.postimg.cc/L6z3HPsM/IMG20230520191533.jpg',
+'https://i.postimg.cc/W1T6t4Dq/IMG20230520191601.jpg',
+'https://i.postimg.cc/q77sXZxk/IMG20230520191611.jpg',
+'https://i.postimg.cc/25JZdj5w/IMG20230520191843.jpg',
+'https://i.postimg.cc/Xq6Bh2dB/IMG20230520191951.jpg',
+'https://i.postimg.cc/xjLzH2Vv/IMG20230520192025.jpg',
+'https://i.postimg.cc/SRLYpWfT/IMG20230520192256.jpg',
+'https://i.postimg.cc/jdXng45C/IMG20230520192258.jpg',
+'https://i.postimg.cc/HkvcHWbg/IMG20230521044019.jpg',
+'https://i.postimg.cc/RhmqGWxc/IMG20230521053952.jpg',]
 }
