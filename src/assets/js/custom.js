0e3c98241217c57c16879c83f14e6376706df061
achyutsharan3267 <achyut.sharan@impressico.com>
2023-08-12 14:37:29 +0530

    page not found

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -480,3 +480,15 @@ return;
 }
 
 
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("nav-menu");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
